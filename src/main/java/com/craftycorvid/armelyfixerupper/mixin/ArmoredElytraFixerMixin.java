831e876fc7f7921b459ce76ad29d12a3efb2e0ff
Ivan0xFF <dev-github@ivanff.com>
2024-04-28 22:02:44 -0700

    first commit

---
+++ src/main/java/com/craftycorvid/armelyfixerupper/mixin/ArmoredElytraFixerMixin.java
@@ -0,0 +1,65 @@
+package com.craftycorvid.armelyfixerupper.mixin;
+
+import net.minecraft.datafixer.fix.ItemStackComponentizationFix;
+import net.minecraft.datafixer.fix.ItemStackComponentizationFix.StackData;
+import java.util.Optional;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.craftycorvid.armelyfixerupper.ArmoredElytraFixerUpper;
+import com.mojang.datafixers.DataFix;
+import com.mojang.datafixers.schemas.Schema;
+import com.mojang.serialization.Dynamic;
+
+@Mixin(ItemStackComponentizationFix.class)
+public abstract class ArmoredElytraFixerMixin extends DataFix {
+
+    protected ArmoredElytraFixerMixin(Schema outputSchema, final boolean changesType) {
+        super(outputSchema, changesType);
+    }
+
+    @Inject(method = "fixStack", at = @At("HEAD"))
+    private static void fixStack(StackData data, Dynamic<?> dynamic, CallbackInfo ci) {
+        if (data.itemEquals("minecraft:elytra")) {
+            Optional<? extends Dynamic<?>> optional = data.getAndRemove("armElyData").result();
+            if (optional.isPresent()) {
+                ArmoredElytraFixerUpper.LOGGER.info("Fixing ArmEly");
+                Dynamic<?> armElyDynamic = (Dynamic<?>) optional.get();
+                Dynamic<?> newArmElyDynamic = fixArmEly(data, armElyDynamic);
+                data.setComponent("minecraft:custom_data", newArmElyDynamic);
+            }
+        }
+    }
+
+    private static Dynamic<?> fixArmEly(StackData data, Dynamic<?> dynamic) {
+        Dynamic<?> armElyDynamic = dynamic.emptyMap();
+        Dynamic<?> dynamic2 = dynamic.emptyMap();
+        Optional<? extends Dynamic<?>> chestplateOptional = dynamic.get("chestplate").result();
+        if (chestplateOptional.isPresent()) {
+            Dynamic<?> chestplateDynamic = (Dynamic<?>) chestplateOptional.get();
+            StackData chestplateData = StackData.fromDynamic(chestplateDynamic).get();
+            ItemStackComponentizationFix.fixStack(chestplateData, chestplateData.nbt);
+            dynamic2 = dynamic2.set("chestplate", chestplateData.finalize());
+        }
+        Optional<? extends Dynamic<?>> elytraOptional = dynamic.get("elytra").result();
+        if (elytraOptional.isPresent()) {
+            Dynamic<?> elytraDynamic = (Dynamic<?>) elytraOptional.get();
+            StackData elytraData = StackData.fromDynamic(elytraDynamic).get();
+            ItemStackComponentizationFix.fixStack(elytraData, elytraData.nbt);
+            dynamic2 = dynamic2.set("elytra", elytraData.finalize());
+        }
+        Optional<? extends Dynamic<?>> materialOptional = dynamic.get("material").result();
+        if (materialOptional.isPresent()) {
+            Dynamic<?> materialDynamic = (Dynamic<?>) materialOptional.get();
+            dynamic2 = dynamic2.set("material", materialDynamic);
+        }
+        Optional<? extends Dynamic<?>> armoredOptional = dynamic.get("armored").result();
+        if (armoredOptional.isPresent()) {
+            Dynamic<?> armoredDynamic = (Dynamic<?>) armoredOptional.get();
+            dynamic2 = dynamic2.set("armored", armoredDynamic);
+        }
+        armElyDynamic = dynamic.set("armElyData", dynamic2);
+        return armElyDynamic;
+    }
+}

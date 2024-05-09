31a62cbf2f0a226ff7d679dc6e2ab6dd22de4717
Ivan0xFF <dev-github@ivanff.com>
2024-05-08 19:04:17 -0700

    Cleaned up logic

--- src/main/java/com/craftycorvid/armelyfixerupper/ArmoredElytraFix.java
+++ src/main/java/com/craftycorvid/armelyfixerupper/ArmoredElytraFix.java
@@ -10,7 +10,6 @@ import com.mojang.serialization.OptionalDynamic;
 import net.minecraft.datafixer.TypeReferences;
 import net.minecraft.datafixer.fix.ItemStackComponentizationFix;
 import net.minecraft.datafixer.fix.ItemStackComponentizationFix.StackData;
-import net.minecraft.datafixer.schema.IdentifierNormalizingSchema;
 
 public class ArmoredElytraFix extends DataFix {
     public ArmoredElytraFix(Schema outputSchema) {
@@ -25,36 +24,25 @@ public class ArmoredElytraFix extends DataFix {
         });
     }
 
+    private static Dynamic<?> newArmElyData;
+
     private static Dynamic<?> fixEmbeddedItems(Dynamic<?> dynamic) {
-        Dynamic<?> armElyDynamic = dynamic.emptyMap();
-        Dynamic<?> dynamic2 = dynamic.emptyMap();
-        Optional<? extends Dynamic<?>> chestplateOptional = dynamic.get("chestplate").result();
-        if (chestplateOptional.isPresent()) {
-            Dynamic<?> chestplateDynamic = chestplateOptional.get();
+        newArmElyData = dynamic.emptyMap();
+        dynamic.get("chestplate").result().ifPresent(chestplateDynamic -> {
             StackData chestplateData = StackData.fromDynamic(chestplateDynamic).get();
             ItemStackComponentizationFix.fixStack(chestplateData, chestplateData.nbt);
-            dynamic2 = dynamic2.set("chestplate", chestplateData.finalize());
-        }
-        Optional<? extends Dynamic<?>> elytraOptional = dynamic.get("elytra").result();
-        if (elytraOptional.isPresent()) {
-            Dynamic<?> elytraDynamic = elytraOptional.get();
+            newArmElyData = newArmElyData.set("chestplate", chestplateData.finalize());
+        });
+        dynamic.get("elytra").result().ifPresent(elytraDynamic -> {
             StackData elytraData = StackData.fromDynamic(elytraDynamic).get();
             ItemStackComponentizationFix.fixStack(elytraData, elytraData.nbt);
-            dynamic2 = dynamic2.set("elytra", elytraData.finalize());
-        }
-        Optional<? extends Dynamic<?>> materialOptional = dynamic.get("material").result();
-        if (materialOptional.isPresent()) {
-            Dynamic<?> materialDynamic = materialOptional.get();
-            dynamic2 = dynamic2.set("material", materialDynamic);
-        }
-        Optional<? extends Dynamic<?>> armoredOptional = dynamic.get("armored").result();
-        if (armoredOptional.isPresent()) {
-            Dynamic<?> armoredDynamic = armoredOptional.get();
-            dynamic2 = dynamic2.set("armored", armoredDynamic);
-        }
-        dynamic2 = dynamic2.set("armElyDataVersion", dynamic2.createInt(3839));
-        armElyDynamic = armElyDynamic.set("armElyData", dynamic2);
-        return armElyDynamic;
+            newArmElyData = newArmElyData.set("elytra", elytraData.finalize());
+        });
+        dynamic.get("material").result().ifPresent(
+                materialDynamic -> newArmElyData = newArmElyData.set("material", materialDynamic));
+        dynamic.get("armored").result().ifPresent(
+                armoredDynamic -> newArmElyData = newArmElyData.set("armored", armoredDynamic));
+        return dynamic.emptyMap().set("armElyData", newArmElyData);
     }
 
 
@@ -62,8 +50,8 @@ public class ArmoredElytraFix extends DataFix {
         return this.writeFixAndRead("Armored Elytra componentization",
                 this.getInputSchema().getType(TypeReferences.ITEM_STACK),
                 this.getOutputSchema().getType(TypeReferences.ITEM_STACK), (dynamic) -> {
-                    Optional<? extends Dynamic<?>> optional = ArmElyStackData.fromDynamic(dynamic)
-                            .filter(data -> data.itemEquals("minecraft:elytra")).map((data) -> {
+                    Optional<? extends Dynamic<?>> optional =
+                            ArmElyStackData.fromDynamic(dynamic).map((data) -> {
                                 fixArmEly(data, dynamic);
                                 return data.finalize2();
                             });
@@ -72,22 +60,24 @@ public class ArmoredElytraFix extends DataFix {
     }
 
     static class ArmElyStackData {
-        private final String itemId;
         private Dynamic<?> dynamic;
         private Dynamic<?> components;
         public Dynamic<?> custom_data;
 
-        private ArmElyStackData(String itemId, Dynamic<?> dynamic) {
-            this.itemId = IdentifierNormalizingSchema.normalize(itemId);
+        private ArmElyStackData(Dynamic<?> dynamic) {
             this.dynamic = dynamic;
             this.components = dynamic.get("components").orElseEmptyMap();
             this.custom_data = components.get("minecraft:custom_data").orElseEmptyMap();
         }
 
         public static Optional<ArmElyStackData> fromDynamic(Dynamic<?> dynamic) {
-            return dynamic.get("id").asString().apply2stable((itemId, count) -> {
-                return new ArmElyStackData(itemId, dynamic);
-            }, dynamic.get("count").asNumber()).result();
+            Boolean isElytra = dynamic.get("id").asString().result()
+                    .filter(i -> i.equals("minecraft:elytra")).isPresent();
+            if (isElytra) {
+                return Optional.of(new ArmElyStackData(dynamic));
+            } else {
+                return Optional.empty();
+            }
         }
 
         public OptionalDynamic<?> getAndRemove(String key) {
@@ -105,10 +95,6 @@ public class ArmoredElytraFix extends DataFix {
             dynamic = dynamic.set("components", this.components);
 
             return dynamic;
-        }
-
-        public boolean itemEquals(String itemId) {
-            return this.itemId.equals(itemId);
         }
     }
 
